a274f412de24f78c98151dc768fe3d11dca3073a
rahulptl556 <rahulptl556@gmail.com>
2024-01-16 22:30:12 +0530

    Middleware, ZOD

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const zod = require('zod');
+
+//Input validator using ZOD
+function login(req,res,next){
+    const schema = zod.object({
+        email : zod.string().email(),
+        password: zod.string()
+        .min(5, { message: "Password must be at least 5 characters long" })
+        .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+        .regex(/[\W_]/, { message: "Password must contain at least one symbol" }),
+    })
+    const input = req.body;
+    const response = schema.safeParse(input);
+    if(response.success){
+        next();
+    }
+    else{
+        res.status(411).json({
+            msg:"Invalid inputs",
+            errors: response.error.errors,
+        })
+    }
+}
+
 app.use(express.json());
 
 //Two Middlewares (userMiddleware & kidneyMiddleware)
@@ -21,7 +45,7 @@ function userMiddleware(req,res,next){
 };
         //kidneyMiddleware
 function kidneyMiddleware(req,res,next){
-    const kidneyId = req.query.kidneyId;
+    const kidneyId = req.query.kidneyId;   
     if(kidneyId === '1' || kidneyId === '2'){
         next();
     }
@@ -42,17 +66,20 @@ app.get('/heart-checkup',userMiddleware,(req,res)=>{
 })
 
 app.post('/health-checkup',(req,res)=>{
-    const kidney = req.body.kidney;
-    const length = kidney.length;
-    res.send("length is " + length);
+    const kidneyId = req.body.kidney;
+    const response = schema.safeParse(kidneyId);
+    res.send(response);
 })
 
+app.post('/login',login,(req,res)=>{
+    res.send('Login sucessful');
+})
 
 //Adding a global catch - error middleware
-app.use((err,req,res,next)=>{
-    res.json({
-        msg:'Some issue on our end, Sorry!'
-    })
-})
+// app.use((err,req,res,next)=>{
+//     res.json({
+//         msg:'Some issue on our end, Sorry!'
+//     })
+// })
 
 app.listen(PORT);
